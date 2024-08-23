e7efdd31ef8972683c729cb102934720a22d3bca
MarcoValeri <marcovaleri@hotmail.it>
2024-08-23 18:06:00 +0100

    Set platform on main

--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import (
 	"eventivicinoame/controllers"
 	"eventivicinoame/database"
 	"net/http"
-	// psh "github.com/platformsh/gohelper"
+
+	psh "github.com/platformsh/gohelper"
 )
 
 func main() {
 	// PlatformSH
-	// platformSH, err := psh.NewPlatformInfo()
-	// if err != nil {
-	// 	panic("Not in a Platform.sh environment")
-	// }
+	platformSH, err := psh.NewPlatformInfo()
+	if err != nil {
+		panic("Not in a Platform.sh environment")
+	}
 
 	// Static files
 	fs := http.FileServer(http.Dir("./public"))
@@ -30,8 +31,8 @@ func main() {
 	database.DatabaseConnection()
 
 	// Local env
-	http.ListenAndServe(":80", nil)
+	// http.ListenAndServe(":80", nil)
 
 	// Platform SH env
-	// http.ListenAndServe(":"+platformSH.Port, nil)
+	http.ListenAndServe(":"+platformSH.Port, nil)
 }
