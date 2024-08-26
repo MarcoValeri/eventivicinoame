bd56f5c892d1c1d22920654fc80579a85e545bb0
MarcoValeri <marcovaleri@hotmail.it>
2024-08-26 11:11:10 +0100

    Add style for sagra template

--- controllers/homeController.go
+++ controllers/homeController.go
@@ -17,8 +17,8 @@ func Home() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		data := PageData{
-			PageTitle:       "Eventi vicino a me",
-			PageDescription: "Add a description",
+			PageTitle:       "Eventi vicino a me: oggi, domani e nel fine settimana",
+			PageDescription: "Eventi vicino a me: sagre, feste, fiere, mercatini, mostre e musei oggi, domani e nel fine settimana, più gli eventi da non perdere il prossimo weekend",
 			CurrentYear:     time.Now().Year(),
 		}
 		tmpl.Execute(w, data)
